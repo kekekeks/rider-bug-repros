fdc13803caf80211c12c9100ac65ea5aa716d33f
Nikita Tsukanov <keks9n@gmail.com>
2019-02-05 10:01:52 +0300

    system linq private

---
+++ SystemPrivateLinq/RiderLinqWtf/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RiderLinqWtf
+{
+    public class Foo
+    {
+        public int Bar { get; set; }
+    }
+    
+    public class Tet
+    {
+        public static int Wat(Dictionary<string, Foo> lst)
+        {
+            return lst.FirstOrDefault(x => x.Value.Bar == 1).Value.Bar;
+        }
+    }
+}
